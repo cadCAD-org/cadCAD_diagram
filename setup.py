be87434ceeebbfc26aa48091f106fe69c3fcd4df
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2020-08-10 16:38:42 -0300

    initial checkpoint

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+long_description = """
+Automated cadCAD system models diagram through GraphViz
+"""
+
+setup(name='cadCAD_diagram',
+      version='0.0.1',
+      description="Diagram tool for cadCAD",
+      long_description=long_description,
+      url='https://github.com/danlessa/cadCAD_diagram',
+      author='Danilo Lessa Bernardineli',
+      author_email='danilo@block.science, danilo.bernardineli@usp.br',
+      packages=find_packages()
+)
